0f7c61873f728dd890d9d10fc91af681eb19419e
Huyen Nguyen <huyen.nguyen.0194@gmail.com>
2019-04-16 10:59:32 -0400

    edit

--- comparativePipeline.py
+++ comparativePipeline.py
@@ -3,7 +3,6 @@ import argparse
 import os
 import subprocess
 import sys
-<<<<<<< HEAD
 import pandas as pd
 import createDiffMatrix
 def calDifference(inFile):
@@ -29,22 +28,10 @@ def main():
     parser.add_argument("-diff",action="store_true", help="calculate differences from cgMLST results")
     parser.add_argument("-i", "--inputFile",help="the path of input File")
     parser.add_argument("-o", "--outFile" ,help="path of output file ") 
-=======
-def cgMLST_call(inFile,outFile,dbFile):
-    #note: mentalist has to be on your path
-    subprocess.call(['mentalist','call','-o',outFile,"--db",dbFile,"-i",inFile])
-    
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--mlst",action="store_true", help="running mentalist")
-    parser.add_argument("-i", "--inputFile", required = True,help="the path of input File")
-    parser.add_argument("-o", "--outFile",required = True, help="path of output file ") 
->>>>>>> 33a406c1fd41ea32f38cbb98e2572192ff7373c7
     parser.add_argument("-db", "--database", help="path of database for cgMLST ") 
     args = parser.parse_args()
     if args.mlst:
         if args.database == None:
-<<<<<<< HEAD
             raise SystemExit("missing database to run mentalist. Exit")
         if args.inputFile==None:
             raise SystemExit("missing input file to run mentalist. Exit")
@@ -56,10 +43,5 @@ def main():
         if args.inputFile==None:
             raise SystemExit("missing the result file of cgMLST calling to calculate the differences. Exit")
         calDifference(args.inputFile)
-=======
-            raise SystemExit("need a database to run mentalist. Exit")
-        cgMLST_call(args.inputFile,args.outFile,args.database)
-
->>>>>>> 33a406c1fd41ea32f38cbb98e2572192ff7373c7
 if __name__ == "__main__":
     main()
